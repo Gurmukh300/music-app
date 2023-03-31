2e78c0571deebce1882b5b8fe3b6928cb616eac1
gurmukh singh <gurmukhs300@gmail.com>
2023-04-01 00:51:29 +1100

    colors added to music

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,45 @@ void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   final player = AudioPlayer();
+
+  void playMusic(int arg) {
+    player.play(AssetSource('note$arg.wav'));
+  }
+
+  Expanded buildKey(Color color, int arg) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playMusic(arg);
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 100, 99, 99),
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('Do it'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
           ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
